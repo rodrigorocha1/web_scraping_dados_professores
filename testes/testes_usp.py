e8a097722edeb8c0c2733a8f585d4eec6ce5897c
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-01-12 16:34:06 -0300

    Pegando nome does site

---
+++ testes/testes_usp.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+url = 'https://pgbioquimica.fmrp.usp.br/orientadores/'
+
+response = requests.get(url, timeout=10, verify=False)
+html = response.text
+site = BeautifulSoup(html, 'html.parser')
+site.title.text
+site.find_all('div', class_='elementor-col-66')[0]
+
+
+for chave, dados in enumerate(site.find_all('div', class_='elementor-col-66')):
+    print('=' * 150)
+    print(chave)
+    print(dados.find('a', text='Ver linhas de pesquisa').get('href'))
+    print('=' * 150)
