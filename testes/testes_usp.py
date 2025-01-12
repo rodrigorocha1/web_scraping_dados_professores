3348d4464f37b498704210fa081f8dfa39b90d75
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-01-12 17:17:43 -0300

    correção url

--- testes/testes_usp.py
+++ testes/testes_usp.py
@@ -18,9 +18,17 @@ for chave, dados in enumerate(site.find_all('div', class_='elementor-col-66')):
     print('=' * 150)
 
 
-url = 'https://pgbioquimica.fmrp.usp.br/maria-eugenia-guazzaroni/'
+url = 'https://pgbioquimica.fmrp.usp.br/roberto-do-nascimento-silva/'
 
 response = requests.get(url, timeout=10, verify=False)
 html = response.text
 site = BeautifulSoup(html, 'html.parser')
-site.find('div', class_='elementor-widget-container').find('a').text
+site.find('div', class_='elementor-widget-container').select('a').text
+
+site.find('div', class_='elementor-widget-container').select('a')[0:2]
+
+
+site.find('div', class_='elementor-widget-container').find('li').text
+
+''.join([a.get_text(strip=True) for a in site.find(
+    'div', class_='elementor-widget-container').select('a')[0:2]])
