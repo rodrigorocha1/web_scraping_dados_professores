1e0080e25dc77e83ceed515e4fe2cba1c8cb8e34
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-01-12 16:43:17 -0300

    obtenção e-mail

--- testes/testes_usp.py
+++ testes/testes_usp.py
@@ -16,3 +16,11 @@ for chave, dados in enumerate(site.find_all('div', class_='elementor-col-66')):
     print(chave)
     print(dados.find('a', text='Ver linhas de pesquisa').get('href'))
     print('=' * 150)
+
+
+url = 'https://pgbioquimica.fmrp.usp.br/maria-eugenia-guazzaroni/'
+
+response = requests.get(url, timeout=10, verify=False)
+html = response.text
+site = BeautifulSoup(html, 'html.parser')
+site.find('div', class_='elementor-widget-container').find('a').text
